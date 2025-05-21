e8d4858832cdd23a3704a5c05aa737b1d0aa1460
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-05-20 21:39:52 -0500

    agregando estilos al card2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <header className=" p-5 ">
+      <header className=" p-5 text-white">
         <div className="flex flex-row rounded-md bg-slate-600 w-full items-center justify-between p-4">
           <div className="flex gap-3">
             <div>logo</div>
@@ -15,32 +15,31 @@ function App() {
       </header>
       <main className="m-10 ">
         <div className="flex flex-wrap gap-4 items-center">
-          <div className="bg-slate-600 w-36 h-28 flex justify-center items-center text-white  rounded-sm  hover:scale-110 transition-transform ease-in-out">
+          <div className="bg-slate-600 w-48 h-[180px] mask-radial-at-center mask-radial-from-50% mask-radial-to-70% flex justify-center items-center text-white  rounded-sm  hover:scale-110 transition-transform ease-in-out">
             open graff0
           </div>
-          <div className="bg-slate-600 w-36 h-28 flex justify-center items-center text-white  rounded-sm  hover:scale-110 transition-transform ease-in-out">
+          <div className="bg-slate-600 w-48 h-[180px] flex justify-center items-center text-white  rounded-sm  hover:scale-110 transition-transform ease-in-out">
             open graff0
           </div>
-          <div className="bg-slate-600 w-36 h-28 flex justify-center items-center text-white  rounded-sm  hover:scale-110 transition-transform ease-in-out">
+          <div className="bg-slate-600 w-48 h-[180px] flex justify-center items-center text-white  rounded-sm  hover:scale-110 transition-transform ease-in-out">
             open graff0
           </div>
-          <div className="bg-slate-600 w-36 h-28 flex justify-center items-center text-white  rounded-sm  hover:scale-110 transition-transform ease-in-out">
+          <div className="bg-slate-600 w-48 h-[180px] flex justify-center items-center text-white  rounded-sm  hover:scale-110 transition-transform ease-in-out">
             open graff0
           </div>
-          <div className="bg-slate-600 w-36 h-28 flex justify-center items-center text-white  rounded-sm  hover:scale-110 transition-transform ease-in-out">
+          <div className="bg-slate-600 w-48 h-[180px] flex justify-center items-center text-white  rounded-sm  hover:scale-110 transition-transform ease-in-out">
             open graff0
           </div>
-          <div className="bg-slate-600 w-36 h-28 flex justify-center items-center text-white  rounded-sm  hover:scale-110 transition-transform ease-in-out">
+          <div className="bg-slate-600 w-48 h-[180px] flex justify-center items-center text-white  rounded-sm  hover:scale-110 transition-transform ease-in-out">
             open graff0
           </div>
-          <div className="bg-slate-600 w-36 h-28 flex justify-center items-center text-white  rounded-sm ">
+          <div className="bg-slate-600 w-48 h-[180px] flex justify-center items-center text-white  rounded-sm ">
             open graff
           </div>
         </div>
-        0
       </main>
 
-      <footer>
+      <footer className="text-center text-white">
         <p>Realizado por Nestor Martinez 2025</p>
       </footer>
     </div>
