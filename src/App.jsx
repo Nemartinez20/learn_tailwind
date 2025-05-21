4b207b8eebc4c1066144a300089ecbba0afc99d1
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-05-20 21:07:31 -0500

    agregando html

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ function App() {
   return (
     <>
       <header className="bg-slate-100">
-        <h1 className="text-2xl">Enlaces de recursos</h1>
-        <p>Aqui encontrarar los recursos de la mds</p>
-
-        <a href="">iconos</a>
+        <div>
+          <div>logo</div>
+          <div>
+            <p>Extensions</p>
+          </div>
+        </div>
+        <div>colorNigut</div>
       </header>
       <main>
         <div className="bg-white">open graff</div>
