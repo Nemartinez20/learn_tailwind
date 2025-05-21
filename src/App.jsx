f4cdee72c01dbb3860545893366e753d5fbd9098
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-05-20 21:21:40 -0500

    agregando estilos

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,39 @@ function App() {
   return (
     <>
       <header className="bg-slate-100">
-        <div>
-          <div>logo</div>
-          <div>
-            <p>Extensions</p>
+        <div className="flex flex-row bg-slate-500 w-full items-center justify-between p-4">
+          <div className="flex gap-3">
+            <div>logo</div>
+            <div>
+              <p>Extensions</p>
+            </div>
           </div>
+
+          <div>colorNigut</div>
         </div>
-        <div>colorNigut</div>
       </header>
-      <main>
-        <div className="bg-white">open graff</div>
+      <main className="m-10 ">
+        <div className="flex flex-wrap gap-4">
+          <div className="bg-slate-300 w-36 h-28  rounded-sm  hover:scale-110 transition-transform ease-in-out">
+            open graff
+          </div>
+          <div className="bg-slate-300 w-36 h-28  rounded-sm  hover:scale-110 transition-transform ease-in-out">
+            open graff
+          </div>
+          <div className="bg-slate-300 w-36 h-28  rounded-sm  hover:scale-110 transition-transform ease-in-out">
+            open graff
+          </div>
+          <div className="bg-slate-300 w-36 h-28  rounded-sm  hover:scale-110 transition-transform ease-in-out">
+            open graff
+          </div>
+          <div className="bg-slate-300 w-36 h-28  rounded-sm  hover:scale-110 transition-transform ease-in-out">
+            open graff
+          </div>
+          <div className="bg-slate-300 w-36 h-28  rounded-sm  hover:scale-110 transition-transform ease-in-out">
+            open graff
+          </div>
+          <div className="bg-slate-300 w-36 h-28  rounded-sm ">open graff</div>
+        </div>
       </main>
 
       <footer>
